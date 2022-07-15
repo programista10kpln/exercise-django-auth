674aad3f7da111d17091e1174ae39c1c800408b1
programista10kpln <programista10kpln@proton.me>
2022-07-15 13:38:26 +0200

    created register site

--- django_auth/urls.py
+++ django_auth/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+from .views import SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls'))
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/register/', SignUpView.as_view(), name="signup"),
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
